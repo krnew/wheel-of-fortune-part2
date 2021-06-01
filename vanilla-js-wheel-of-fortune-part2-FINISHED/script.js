d90eb68db6f0404acf264c007bf09c639d134297
krnew <n.kritsanewzzz@gmail.com>
2021-06-01 14:53:57 +0700

    Update script.js

--- vanilla-js-wheel-of-fortune-part2-FINISHED/script.js
+++ vanilla-js-wheel-of-fortune-part2-FINISHED/script.js
@@ -3,35 +3,21 @@
 (function() {
   const wheel = document.querySelector('.wheel');
   const startButton = document.querySelector('.button');
-  const display = document.querySelector('.display');
+  
   
   let deg = 0;
   let zoneSize = 45; // deg
 
-  // Counter clockwise
-  const symbolSegments = {
-    1: "Frog",
-    2: "Snail",
-    3: "Dolphin",
-    4: "Ladybug",
-    5: "Koala",
-    6: "Unicorn",
-    7: "Dragon",
-    8: "Snowman",
-  }
-
   const handleWin = (actualDeg) => {
     const winningSymbolNr = Math.ceil(actualDeg / zoneSize);
-    display.innerHTML = symbolSegments[winningSymbolNr];
   }
 
   startButton.addEventListener('click', () => {
-    // Reset display
-    display.innerHTML = "-";
+    
     // Disable button during spin
     startButton.style.pointerEvents = 'none';
     // Calculate a new rotation between 5000 and 10 000
-    deg = Math.floor(5000 + Math.random() * 5000);
+    deg = Math.floor(5000 + 5000);
     // Set the transition on the wheel
     wheel.style.transition = 'all 10s ease-out';
     // Rotate the wheel
@@ -42,8 +28,6 @@
 
   wheel.addEventListener('transitionend', () => {
     // Remove blur
-    wheel.classList.remove('blur');
-    // Enable button when spin is over
     startButton.style.pointerEvents = 'auto';
     // Need to set transition to none as we want to rotate instantly
     wheel.style.transition = 'none';
